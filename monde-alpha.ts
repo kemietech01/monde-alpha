8573bafe7ad3f0ed284defd880e90398da9e6acc
kemietech01 <kevin@jimmybox.fr>
2023-10-14 22:42:24 +0800

    intégration éléments de base three.js

---
+++ monde-alpha.ts
@@ -0,0 +1,33 @@
+import * as thr from 'three';
+
+const scene_1 = new thr.Scene();
+
+const camera_1 = new thr.PerspectiveCamera();
+
+const geometrie_entite_1 = new thr.BoxGeometry(
+    1,
+    1,
+    1
+);
+
+const materiau_entite_1 = new thr.MeshBasicMaterial({
+    color: 0x00ff00
+});
+
+const maillage_entite_1 = new thr.Mesh(
+    geometrie_entite_1,
+    materiau_entite_1
+);
+
+const lumiere_1 = new thr.AmbientLight(
+    "white",
+    1.5
+);
+
+scene_1.add(maillage_entite_1);
+scene_1.add(lumiere_1);
+scene_1.add(camera_1);
+
+camera_1.position.z =- 3;
+
+const rendu_1 = new thr.WebGLRenderer();
