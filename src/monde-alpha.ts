0b95e58d7bd59efd985ac5595fc0b53353344e14
kemietech01 <kevin@jimmybox.fr>
2023-10-15 00:51:34 +0800

    ajout éléments minimaux de scène

--- src/monde-alpha.ts
+++ src/monde-alpha.ts
@@ -2,7 +2,12 @@ import * as thr from 'three';
 
 const scene_1 = new thr.Scene();
 
-const camera_1 = new thr.PerspectiveCamera();
+const camera_1 = new thr.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
 const geometrie_entite_1 = new thr.BoxGeometry(
     1,
@@ -11,7 +16,8 @@ const geometrie_entite_1 = new thr.BoxGeometry(
 );
 
 const materiau_entite_1 = new thr.MeshBasicMaterial({
-    color: 0x00ff00
+    color: 0x00ff00,
+    wireframe: true
 });
 
 const maillage_entite_1 = new thr.Mesh(
@@ -28,8 +34,26 @@ scene_1.add(maillage_entite_1);
 scene_1.add(lumiere_1);
 scene_1.add(camera_1);
 
-camera_1.position.z =- 3;
+camera_1.position.z = 3;
 
 const rendu_1 = new thr.WebGLRenderer();
 
+rendu_1.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
+
+document.body.appendChild(rendu_1.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    rendu_1.render(
+        scene_1,
+        camera_1
+    );
+}
+
+
+animate();
 console.log("test");
